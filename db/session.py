8f93b6905b6fde8d308ff5bb3e333d531abec9ca
MikhailKochetkov <consultant.dax@gmail.com>
2023-08-10 20:19:49 +0300

    Refactoring

--- db/session.py
+++ db/session.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
-from .models import Menu, SubMenu, Dish
 from .db_connection import PG_CONNECTION_STRING
 from settings import DEV_MODE
 
@@ -8,12 +7,6 @@ from settings import DEV_MODE
 if DEV_MODE:
     engine = create_async_engine(PG_CONNECTION_STRING, future=True)
     async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-
-    async def create_tables():
-        async with engine.begin() as conn:
-            await conn.run_sync(Menu.metadata.create_all)
-            await conn.run_sync(SubMenu.metadata.create_all)
-            await conn.run_sync(Dish.metadata.create_all)
 
     async def get_session():
         async with async_session() as session:
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import asyncio
 from fastapi import FastAPI, APIRouter
 
 from api.menu_handlers import menu_router
 from api.submenu_handlers import submenu_router
 from api.dish_handlers import dish_router
-from settings import DEV_MODE
 
 
 app = FastAPI(title='Restaurant menu')
@@ -14,8 +12,3 @@ main_router.include_router(menu_router)
 main_router.include_router(submenu_router)
 main_router.include_router(dish_router)
 app.include_router(main_router)
-
-if DEV_MODE:
-    from db.session import create_tables
-
-    asyncio.create_task(create_tables())
