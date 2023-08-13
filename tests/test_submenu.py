373c062597a0cd411bd91ca31b289c996519276c
MikhailKochetkov <consultant.dax@gmail.com>
2023-08-13 13:52:05 +0300

    Add tests submenu

--- tests/test_submenu.py
+++ tests/test_submenu.py
@@ -36,10 +36,25 @@ async def test_create_submenu(client):
     assert submenu_response.status_code == status.HTTP_404_NOT_FOUND
 
 
-@pytest.mark.skip(reason='no way of currently testing this')
 @pytest.mark.asyncio
 async def test_get_submenus(client):
-    pass
+    menu = {
+        "title": "test menu 6",
+        "description": "test menu description 6"
+    }
+    menu_response = await client.post('/api/v1/menus', json=menu)
+    menu_id = menu_response.json()["id"]
+    submenu = {
+        "title": "test submenu 5",
+        "description": "test submenu description 5",
+        "menu_id": f"{menu_id}"
+    }
+    submenu_response = await client.post(f'/api/v1/menus/{menu_id}/submenus', json=submenu)
+    get_submenu_response = await client.get(f'/api/v1/menus/{menu_id}/submenus')
+    assert get_submenu_response.status_code == status.HTTP_200_OK
+    assert get_submenu_response.json()[0]["id"] == submenu_response.json()["id"]
+    assert get_submenu_response.json()[0]["title"] == submenu_response.json()["title"]
+    assert get_submenu_response.json()[0]["description"] == submenu_response.json()["description"]
 
 
 @pytest.mark.skip(reason='no way of currently testing this')
