c6dbcb88f6055e341c4d126ebd182060f4c4e3da
MikhailKochetkov <consultant.dax@gmail.com>
2023-08-13 17:22:19 +0300

    Add tests

--- tests/test_submenu.py
+++ tests/test_submenu.py
@@ -55,15 +55,68 @@ async def test_get_submenu_by_id(client):
     assert get_submenu_response.json()["id"] == submenu_response.json()["id"]
     assert get_submenu_response.json()["title"] == submenu_response.json()["title"]
     assert get_submenu_response.json()["description"] == submenu_response.json()["description"]
+    menu_id = "2aa-3bb"
+    submenu_id = "4cc-5dd"
+    get_submenu_response = await client.get(f'/api/v1/menus/{menu_id}/submenus/{submenu_id}')
+    assert get_submenu_response.status_code == status.HTTP_404_NOT_FOUND
 
 
-@pytest.mark.skip(reason='no way of currently testing this')
 @pytest.mark.asyncio
 async def test_update_submenu(client):
-    pass
+    menu = create_unique_menu()
+    menu_response = await client.post('/api/v1/menus', json=menu)
+    real_menu_id = menu_response.json()["id"]
+    submenu = create_unique_submenu(real_menu_id)
+    submenu_response = await client.post(
+        f'/api/v1/menus/{real_menu_id}/submenus',
+        json=submenu
+    )
+    real_submenu_id = submenu_response.json()["id"]
+    upd_submenu = {
+        "title": "updated test submenu title",
+        "description": "updated est submenu description"
+    }
+    patch_response = await client.patch(
+        f'/api/v1/menus/{real_menu_id}/submenus/{real_submenu_id}',
+        json=upd_submenu
+    )
+    patch_data = patch_response.json()
+    assert patch_response.status_code == status.HTTP_200_OK
+    assert patch_data["id"] == real_submenu_id
+    assert patch_data["title"] == upd_submenu["title"]
+    assert patch_data["description"] == upd_submenu["description"]
+    menu_id = real_menu_id
+    submenu_id = "12-aa-21"
+    patch_response = await client.patch(
+        f'/api/v1/menus/{menu_id}/submenus/{submenu_id}',
+        json=upd_submenu
+    )
+    assert patch_response.status_code == status.HTTP_404_NOT_FOUND
+    assert patch_response.json()["detail"] == "submenu not found"
+    menu_id = "33-ff-44"
+    submenu_id = real_submenu_id
+    patch_response = await client.patch(
+        f'/api/v1/menus/{menu_id}/submenus/{submenu_id}',
+        json=upd_submenu
+    )
+    assert patch_response.status_code == status.HTTP_404_NOT_FOUND
+    assert patch_response.json()["detail"] == "menu does not exist"
 
 
-@pytest.mark.skip(reason='no way of currently testing this')
 @pytest.mark.asyncio
-async def test_delete_submenu():
-    pass
+async def test_delete_submenu(client):
+    menu = create_unique_menu()
+    menu_response = await client.post('/api/v1/menus', json=menu)
+    real_menu_id = menu_response.json()["id"]
+    submenu = create_unique_submenu(real_menu_id)
+    submenu_response = await client.post(
+        f'/api/v1/menus/{real_menu_id}/submenus',
+        json=submenu
+    )
+    real_submenu_id = submenu_response.json()["id"]
+    del_response = await client.delete(f'/api/v1/menus/{real_menu_id}/submenus/{real_submenu_id}')
+    assert del_response.json()["message"] == "submenu deleted successfully"
+    submenu_id = "23-yy-99"
+    del_response = await client.delete(f'/api/v1/menus/{real_menu_id}/submenus/{submenu_id}')
+    assert del_response.status_code == status.HTTP_404_NOT_FOUND
+    assert del_response.json()["detail"] == "submenu not found"
