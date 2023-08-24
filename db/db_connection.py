ea16976a8137fa96df8048c5805c5960c67a4b17
MikhailKochetkov <consultant.dax@gmail.com>
2023-08-24 09:37:56 +0300

    flake8

--- db/db_connection.py
+++ db/db_connection.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-PG_CONNECTION_STRING = (f'{os.getenv("PG_DB_URL", default="postgresql+asyncpg://")}'
+PG_CONNECTION_STRING = (f'{os.getenv("PG_DB_URL", default="postgresql")}'
                         f'{os.getenv("POSTGRES_USER", default="postgres")}:'
                         f'{os.getenv("POSTGRES_PASSWORD", default="postgres")}'
                         f'@'
-                        f'{os.getenv("DB_HOST", default="db")}:'
+                        f'{os.getenv("DB_HOST", default="localhost")}:'
                         f'{os.getenv("DB_PORT", default=5432)}/'
                         f'{os.getenv("POSTGRES_DB", default="database")}')
