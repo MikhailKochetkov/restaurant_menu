b77df10e5ca8f852ad8415098c52ead11f5562ed
MikhailKochetkov <consultant.dax@gmail.com>
2023-08-27 20:34:23 +0300

    Some changes added

--- api/menu_handlers.py
+++ api/menu_handlers.py
@@ -27,7 +27,7 @@ async def create_menu(
             title=request.title,
             description=request.description
         )
-    except ValidationError as e:
+    except ValidationError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail='the data are not valid'
