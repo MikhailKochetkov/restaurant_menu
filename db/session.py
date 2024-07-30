90dd0b13e5f83d7d93bc8796cbf66086763b751b
MikhailKochetkov <consultant.dax@gmail.com>
2024-07-30 22:33:17 +0300

    Refactoring

--- db/session.py
+++ db/session.py
@@ -1,18 +1,13 @@
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    async_sessionmaker,
-    create_async_engine)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from settings import DEV_MODE
 from .db_connection import CONNECTION_STRING
 
+engine = create_async_engine(CONNECTION_STRING, pool_pre_ping=True)
+async_session = async_sessionmaker(
+    bind=engine, expire_on_commit=False, class_=AsyncSession
+)
 
-if DEV_MODE:
-    engine = create_async_engine(CONNECTION_STRING, pool_pre_ping=True)
-    async_session = async_sessionmaker(
-        bind=engine, expire_on_commit=False, class_=AsyncSession
-    )
 
-    async def get_session() -> AsyncSession:
-        async with async_session() as session:
-            yield session
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
